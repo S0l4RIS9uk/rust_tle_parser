cd94e0d18de73c58f8032731bd7fd46a87c77944
S0l4RIS9uk <115390498+S0l4RIS9uk@users.noreply.github.com>
2024-06-20 04:18:56 +0000

    updated output test files

--- src/main.rs
+++ src/main.rs
@@ -95,12 +95,14 @@ async fn main() -> Result<()> {
     println!("{}", lines[0].to_string());
     println!("{}", parse_tle(&lines[0]).to_string());
     let parsed = lines.iter().map(parse_tle);
-    if fs::metadata("./test.txt").is_ok() {
-        write("./test.txt", parsed.clone().map(|tle: TLE| tle.to_string()).collect::<Vec<String>>().join("\n\n"))?;
+    if !fs::metadata("./output").is_ok() {
+        fs::create_dir("./output").expect("Could not create output dir.");
+        write("./output/test.txt", parsed.clone().map(|tle: TLE| tle.to_string()).collect::<Vec<String>>().join("\n\n"))?;
+        write("./output/test.json", serde_json::to_string_pretty(&parsed.collect::<Vec<TLE>>()).expect("Failed to serialise TLE's"))?;
     } else {
-        write("./test.txt", parsed.clone().map(|tle: TLE| tle.to_string()).collect::<Vec<String>>().join("\n\n"))?;
+        write("./output/test.txt", parsed.clone().map(|tle: TLE| tle.to_string()).collect::<Vec<String>>().join("\n\n"))?;
+        write("./output/test.json", serde_json::to_string_pretty(&parsed.collect::<Vec<TLE>>()).expect("Failed to serialise TLE's"))?;
     }
-    write("./test.json", serde_json::to_string_pretty(&parsed.collect::<Vec<TLE>>()).expect("Failed to serialise TLE's"))?;
 
     Ok(())
 }
